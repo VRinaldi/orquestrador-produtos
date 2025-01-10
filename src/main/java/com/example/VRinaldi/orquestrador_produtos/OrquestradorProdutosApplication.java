3e5b0a2aae65678a47f09d11d211e3d16f52d407
VRinaldi <vrinaldi2001@hotmail.com>
2025-01-09 21:36:59 -0300

    Initial commit

---
+++ src/main/java/com/example/VRinaldi/orquestrador_produtos/OrquestradorProdutosApplication.java
@@ -0,0 +1,13 @@
+package com.example.VRinaldi.orquestrador_produtos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrquestradorProdutosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrquestradorProdutosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/VRinaldi/orquestrador_produtos/OrquestradorProdutosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.VRinaldi.orquestrador_produtos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrquestradorProdutosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
